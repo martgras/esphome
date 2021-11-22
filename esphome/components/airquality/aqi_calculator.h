c32d7ae8c8371ea954a061441f8aad6f6e66ad1b
Martin <25747549+martgras@users.noreply.github.com>
2021-11-22 23:16:12 +0100

    fix negative values

--- esphome/components/airquality/aqi_calculator.h
+++ esphome/components/airquality/aqi_calculator.h
@@ -10,6 +10,9 @@ class AQICalculator : public AbstractAQICalculator {
  public:
   uint8_t calculate_index(uint16_t value, Pollutant pollutant_type) override {
     int grid_index = get_grid_index_(value, pollutant_type);
+    // max level eceeded
+    if (grid_index == AMOUNT_OF_LEVELS)
+      return index_grid_[AMOUNT_OF_LEVELS - 1][1];
     int aqi_lo = index_grid_[grid_index][0];
     int aqi_hi = index_grid_[grid_index][1];
     int conc_lo = calculation_grids_[pollutant_type][grid_index][0];
@@ -24,17 +27,17 @@ class AQICalculator : public AbstractAQICalculator {
   // Note must be defined using the order of enum Pollutant
   const int calculation_grids_[(Pollutant::SO2 + 1)][AMOUNT_OF_LEVELS][2] = {
       // pm2_5 24h avg
-      {{0, 12}, {13, 35}, {36, 55}, {56, 150}, {151, 250}, {251, 350}, {351, 0x7FFF}},
+      {{0, 12}, {13, 35}, {36, 55}, {56, 150}, {151, 250}, {251, 350}, {351, 500}},
       // PM 10 24h avg
-      {{0, 54}, {55, 154}, {155, 254}, {255, 354}, {355, 424}, {425, 504}, {425, 0x7FFF}},
+      {{0, 54}, {55, 154}, {155, 254}, {255, 354}, {355, 424}, {425, 504}, {425, 604}},
       // No2 1h avr
-      {{0, 53}, {54, 100}, {101, 360}, {361, 649}, {650, 1249}, {1250, 1649}, {1650, 0x7FFF}},
+      {{0, 53}, {54, 100}, {101, 360}, {361, 649}, {650, 1249}, {1250, 1649}, {1650, 2049}},
       //  o3  not exaclty correct AQI uses 8h and 1h avg for
-      {{0, 54}, {55, 124}, {125, 164}, {165, 204}, {205, 404}, {405, 504}, {505, 0x7FFF}},
+      {{0, 54}, {55, 124}, {125, 164}, {165, 204}, {205, 404}, {405, 504}, {505, 604}},
       // CO - 8h avg
-      {{0, 4}, {5, 9}, {10, 12}, {13, 14}, {15, 30}, {31, 40}, {41, 0x7FFF}},
+      {{0, 4}, {5, 9}, {10, 12}, {13, 14}, {15, 30}, {31, 40}, {41, 50}},
       // SO2 1h avg
-      {{0, 35}, {36, 75}, {76, 185}, {186, 304}, {305, 604}, {605, 804}, {805, 0x7FFF}}};
+      {{0, 35}, {36, 75}, {76, 185}, {186, 304}, {305, 604}, {605, 804}, {805, 1004}}};
 
   int get_grid_index_(uint16_t value, Pollutant pollutant_type) {
     for (int i = 0; i < AMOUNT_OF_LEVELS; i++) {
@@ -42,7 +45,7 @@ class AQICalculator : public AbstractAQICalculator {
         return i;
       }
     }
-    return -1;
+    return AMOUNT_OF_LEVELS;
   }
 };  // namespace airquality
 
--- esphome/components/airquality/caqi_calculator.h
+++ esphome/components/airquality/caqi_calculator.h
@@ -10,6 +10,8 @@ class CAQICalculator : public AbstractAQICalculator {
  public:
   uint8_t calculate_index(uint16_t value, Pollutant pollutant_type) override {
     int grid_index = get_grid_index_(value, pollutant_type);
+    if (grid_index == AMOUNT_OF_LEVELS)
+      return index_grid_[AMOUNT_OF_LEVELS - 1][1];
     int aqi_lo = index_grid_[grid_index][0];
     int aqi_hi = index_grid_[grid_index][1];
     int conc_lo = calculation_grids_[pollutant_type][grid_index][0];
@@ -20,26 +22,22 @@ class CAQICalculator : public AbstractAQICalculator {
  protected:
   static const int AMOUNT_OF_LEVELS = 5;
 
-  int index_grid_[AMOUNT_OF_LEVELS][2] = {{0, 25}, {26, 50}, {51, 75}, {76, 100}, {101, 400}};
-
-  int pm2_5_calculation_grid_[AMOUNT_OF_LEVELS][2] = {{0, 15}, {16, 30}, {31, 55}, {56, 110}, {111, 400}};
-
-  int pm10_0_calculation_grid_[AMOUNT_OF_LEVELS][2] = {{0, 25}, {26, 50}, {51, 90}, {91, 180}, {181, 400}};
+  int index_grid_[AMOUNT_OF_LEVELS][2] = {{0, 25}, {26, 50}, {51, 75}, {76, 100}, {101, 600}};
 
   // Note must be defined using the order of enum Pollutant
   const int calculation_grids_[(Pollutant::SO2 + 1)][AMOUNT_OF_LEVELS][2] = {
       // PM25
-      {{0, 15}, {16, 30}, {31, 55}, {56, 110}, {111, 0x7FFF}},
+      {{0, 15}, {16, 30}, {31, 55}, {56, 110}, {111, 600}},
       // PM 10
-      {{0, 25}, {26, 50}, {51, 90}, {91, 180}, {181, 0x7FFF}},
+      {{0, 25}, {26, 50}, {51, 90}, {91, 180}, {181, 600}},
       // No2
-      {{0, 50}, {51, 100}, {101, 200}, {201, 400}, {400, 0x7FFF}},
+      {{0, 50}, {51, 100}, {101, 200}, {201, 400}, {400, 600}},
       //  O3
-      {{0, 60}, {61, 120}, {121, 180}, {181, 240}, {241, 0x7FFF}},
+      {{0, 60}, {61, 120}, {121, 180}, {181, 240}, {241, 600}},
       // CO - 8h avg
-      {{0, 5}, {6, 8}, {9, 10}, {11, 20}, {21, 0x7FFF}},
+      {{0, 5}, {6, 8}, {9, 10}, {11, 20}, {21, 600}},
       // SO2 1h avg
-      {{0, 50}, {51, 100}, {101, 350}, {350, 500}, {501, 0x7FFF}}};
+      {{0, 50}, {51, 100}, {101, 350}, {350, 500}, {501, 600}}};
 
   int get_grid_index_(uint16_t value, Pollutant pollutant_type) {
     for (int i = 0; i < AMOUNT_OF_LEVELS; i++) {
@@ -47,7 +45,7 @@ class CAQICalculator : public AbstractAQICalculator {
         return i;
       }
     }
-    return -1;
+    return AMOUNT_OF_LEVELS;
   }
 };
 
