0183c2f521ca3bee0e7e925fb1a62c5e76de9c70
Martin <25747549+martgras@users.noreply.github.com>
2021-12-02 16:03:34 +0100

    fix statics

--- esphome/components/airquality/aqi_calculator.cpp
+++ esphome/components/airquality/aqi_calculator.cpp
@@ -3,8 +3,8 @@
 namespace esphome {
 namespace airquality {
 
-const constexpr int AQICalc::index_grid_[AQICalc::AMOUNT_OF_LEVELS][2];
-const constexpr int AQICalc::calculation_grids_[(Pollutant::SO2 + 1)][AQICalc::AMOUNT_OF_LEVELS][2];
+//const constexpr int AQICalc::index_grid_[AQICalc::AMOUNT_OF_LEVELS][2];
+//const constexpr int AQICalc::calculation_grids_[(Pollutant::SO2 + 1)][AQICalc::AMOUNT_OF_LEVELS][2];
 
 }  // namespace airquality
 }  // namespace esphome
